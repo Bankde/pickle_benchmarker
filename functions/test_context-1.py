dc24f0ca1e0ad66a2c692354a4964d3cd507aefb
Bankde <bankde@hotmail.com>
2022-02-10 16:15:31 +0700

    improve test on context object

--- functions/test_context-1.py
+++ functions/test_context-1.py
@@ -17,6 +17,8 @@ import helper
 ########## Code Here ##########
 
 from decimal import Decimal, localcontext
+import io
+
 def function_with_context(num, prec):
     with localcontext() as ctx:
         ctx.prec = prec
@@ -24,17 +26,47 @@ def function_with_context(num, prec):
 
 contextManager1 = localcontext()
 
+class UserContextManager(object):
+    def __init__(self, logger):
+        self.logger = logger
+
+    def __enter__(self):
+        self.logger.write("Enter context\n")
+        return self.logger
+
+    def __exit__(self, type, value, traceback):
+        self.logger.write("Exit context\n")
+
 class Test(helper.PickleTest):
     def test_function_with_context(self):
-        self.assertEqual(function_with_context(41, 30), Decimal("0.0243902439024390243902439024390"))
+        self.assertEqual(function_with_context(41, 50), Decimal("0.024390243902439024390243902439024390243902439024390"))
         self.obj['f1'] = self.dumps(function_with_context)
 
     def test_context_manager(self):
         with contextManager1 as ctx:
-            ctx.prec = 30
+            ctx.prec = 50
             result = Decimal("1") / Decimal("41")
-        self.assertEqual(result, Decimal("0.0243902439024390243902439024390"))
+        self.assertEqual(result, Decimal("0.024390243902439024390243902439024390243902439024390"))
         self.obj['c1'] = self.dumps(contextManager1)
+
+    def test_user_context_manager(self):
+        logger = io.StringIO()
+        with UserContextManager(logger) as log:
+            self.assertEqual(logger.getvalue(), "Enter context\n")
+            log.write("Hello\n")
+            self.assertEqual(logger.getvalue(), "Enter context\nHello\n")
+        self.assertEqual(logger.getvalue(), "Enter context\nHello\nExit context\n")
+        self.obj['c1'] = self.dumps(UserContextManager)
+
+        logger2 = io.StringIO()
+        self.obj['c2'] = self.dumps(UserContextManager(logger2))
+
+    def test_context(self):
+        with contextManager1 as ctx:
+            ctx.prec = 50
+            result = Decimal("1") / Decimal("41")
+            self.assertEqual(result, Decimal("0.024390243902439024390243902439024390243902439024390"))
+            self.obj['c1'] = self.dumps(ctx)
 
 ########## End of Code ##########
 
--- functions/test_context-2.py
+++ functions/test_context-2.py
@@ -8,19 +8,41 @@ import helper
 
 ########## Code Here ##########
 
-from decimal import Decimal, localcontext
+from decimal import Decimal
+import io
 
 class Test(helper.PickleTest):
     def test_function_with_context(self):
         function_with_context = self.loads(self.obj['f1'])
-        self.assertEqual(function_with_context(41, 30), Decimal("0.0243902439024390243902439024390"))
+        self.assertEqual(function_with_context(41, 50), Decimal("0.024390243902439024390243902439024390243902439024390"))
 
     def test_context_manager(self):
         contextManager1 = self.loads(self.obj['c1'])
         with contextManager1 as ctx:
-            ctx.prec = 30
+            ctx.prec = 50
             result = Decimal("1") / Decimal("41")
-        self.assertEqual(result, Decimal("0.0243902439024390243902439024390"))
+        self.assertEqual(result, Decimal("0.024390243902439024390243902439024390243902439024390"))
+
+    def test_user_context_manager(self):
+        logger = io.StringIO()
+        UserContextManager = self.loads(self.obj['c1'])
+        with UserContextManager(logger) as log:
+            self.assertEqual(logger.getvalue(), "Enter context\n")
+            log.write("Hello\n")
+            self.assertEqual(logger.getvalue(), "Enter context\nHello\n")
+        self.assertEqual(logger.getvalue(), "Enter context\nHello\nExit context\n")
+
+        with self.loads(self.obj['c2']) as log:
+            self.assertEqual(log.getvalue(), "Enter context\n")
+            log.write("Hello\n")
+            self.assertEqual(log.getvalue(), "Enter context\nHello\n")
+
+    def test_context(self):
+        # This doesn't look like a correct way to test context but we couldn't find the way to test it.
+        # It's weird we can __reduce__ ctx object, we shouldn't be able to.
+        ctx = self.loads(self.obj['c1'])
+        result = Decimal("1") / Decimal("41")
+        self.assertEqual(result, Decimal("0.024390243902439024390243902439024390243902439024390"))
 
 ########## End of Code ##########
 
